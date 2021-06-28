c0dcf5c31129b1cbdf096f2c1465b90f3eb6bfb0
milanfilipovicnp <milanfilipovicnp@hotmail.com>
2021-06-28 18:17:18 +0200

    first commit

---
+++ Task6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = "abb";
+            int n = str.Length;
+            permute(str, 0, n - 1);
+
+
+        }
+        private static void permute(String str,
+                               int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        public static String swap(String a,
+                              int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+    }
+}
